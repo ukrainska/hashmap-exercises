224b998c5e910f182ea85df20990fb2e0b30c0d9
ukrainska <215995465+ukrainska@users.noreply.github.com>
2026-05-31 23:42:31 +0900

    Create a hashMap class base

---
+++ hashMapEx.js
@@ -0,0 +1,18 @@
+class HashMap {
+    constructor () {
+        this.loadFactor = 0.75;
+        this.capacity = 16;
+    }
+
+    hash(key) {
+        let hashCode = 0;
+
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+            hashCode = primeNumber * hashCode + key.charCodeAt(i);
+        }
+
+        return hashCode;
+    } 
+
+}
